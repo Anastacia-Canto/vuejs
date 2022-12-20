0052a84d5eaefb632320d7577c4cf4401af71380
Anastacia <anaanestesia@gmail.com>
2022-12-20 18:36:32 +0000

    organizing topics on pages

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,10 @@
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="text-h6">
-            Application
+            Todo List
           </v-list-item-title>
           <v-list-item-subtitle>
-            subtext
+            diary tasks
           </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
@@ -24,6 +24,7 @@
         <v-list-item
           v-for="item in items"
           :key="item.title"
+		  :to="item.to"
           link
         >
           <v-list-item-icon>
@@ -40,17 +41,23 @@
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Todo List</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <!--  -->
-    </v-main>
+		<router-view></router-view>
+	</v-main>
   </v-app>
 </template>
 
 <script>
   export default {
-    data: () => ({ drawer: null }),
+    data: () => ({ 
+		drawer: null,
+		items: [
+		  { title: 'Todo', icon: 'mdi-help-box', to:'/' },
+          { title: 'About', icon: 'mdi-help-box', to:'/about' }
+        ]
+	}),
   }
 </script>
