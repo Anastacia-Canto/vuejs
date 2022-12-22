1480c50c9af92f4959ff17310b90593c878d6ccb
Anastacia <anaanestesia@gmail.com>
2022-12-22 10:37:19 +0000

    finished with video content

--- src/App.vue
+++ src/App.vue
@@ -38,10 +38,37 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar
+      color="primary"
+      dark
+      prominent
+      src="mountains.jpg"
+	  app
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Todo List</v-toolbar-title>
+      <v-app-bar-title>Todo List</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
@@ -55,7 +82,7 @@
     data: () => ({ 
 		drawer: null,
 		items: [
-		  { title: 'Todo', icon: 'mdi-help-box', to:'/' },
+		  { title: 'Todo', icon: 'mdi-format-list-checks', to:'/' },
           { title: 'About', icon: 'mdi-help-box', to:'/about' }
         ]
 	}),
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-5">
+    <h1>About Vuetify Todo</h1>
+	<p>This is a test to learn Vuetify skills</p>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quis aliquid similique reiciendis eaque, iusto commodi consequatur debitis ut unde. Rem officiis in possimus recusandae, unde vel? Tenetur, voluptatem assumenda.</p>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quis aliquid similique reiciendis eaque, iusto commodi consequatur debitis ut unde. Rem officiis in possimus recusandae, unde vel? Tenetur, voluptatem assumenda.</p>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quis aliquid similique reiciendis eaque, iusto commodi consequatur debitis ut unde. Rem officiis in possimus recusandae, unde vel? Tenetur, voluptatem assumenda.</p>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quis aliquid similique reiciendis eaque, iusto commodi consequatur debitis ut unde. Rem officiis in possimus recusandae, unde vel? Tenetur, voluptatem assumenda.</p>
   </div>
 </template>
